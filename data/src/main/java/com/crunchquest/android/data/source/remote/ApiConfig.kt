fd3837f257a65a38ce1c8829890aa69b0b555ffe
Satriagucci <fngevnthppv@gmail.com>
2024-10-01 12:14:09 +0700

    reconfigure setup api

--- data/src/main/java/com/crunchquest/android/data/source/remote/ApiConfig.kt
+++ data/src/main/java/com/crunchquest/android/data/source/remote/ApiConfig.kt
@@ -6,11 +6,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiConfig {
-    private const val BASE_URL = "https://crunchquest-api-e8fa3c3bab7e.herokuapp.com/"
+    private const val ML_URL = "https://crunchquest-api-e8fa3c3bab7e.herokuapp.com/"
     private const val PAYMENT_URL = "https://cq-payment-1425524eb51a.herokuapp.com/"
-//    private const val PAYMENT_URL = "http://192.168.100.42:8000/"
-    private const val DB_API_URL = "http://192.168.100.42:3000/"
-
+    private const val DB_API_URL = "https://cq-connection-api-1f85877da6db.herokuapp.com/"
 
     private val client by lazy {
         val logging = HttpLoggingInterceptor().apply {
@@ -21,13 +19,13 @@ object ApiConfig {
             .build()
     }
 
-//    val retrofit: Retrofit by lazy {
-//        Retrofit.Builder()
-//            .baseUrl(BASE_URL)
-//            .client(client)
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//    }
+    val retrofitAi: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(ML_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 
     val retrofitPayment: Retrofit by lazy {
         Retrofit.Builder()
